f3392a411346c42e125fb9aee9592a3684fd468c
Fabrizio <fabrizio_zuniga2003@hotmail.com>
2023-09-07 15:39:12 -0500

    Editando comentarios python

--- Tipos/02-string.py
+++ Tipos/02-string.py
@@ -7,26 +7,26 @@ Bien Hecho
 """
 print(nombre_curso,descripcion)
 
-"""Cantidad de caracteres total"""
+#"""Cantidad de caracteres total"""
 
 print(len(nombre_curso))
 
-"Traer solo en el orden marcado"
+#"Traer solo en el orden marcado"
 
 print(nombre_curso[0])
 
-"Delimitando cuando digitos traer"
+#"Delimitando cuando digitos traer"
 
 print(nombre_curso[0:8])
 
-"Cuando no se pasa el valo de la derecha va hasta el final"
+#"Cuando no se pasa el valo de la derecha va hasta el final"
 
 print(nombre_curso[9:])
 
-"Cuando no se pasa el valo de la izquierda asume un valor 0"
+#"Cuando no se pasa el valo de la izquierda asume un valor 0"
 
 print(nombre_curso[:8])
 
-"Asime valores por defecto desde 0 hasta el ultimo valor"
+#"Asime valores por defecto desde 0 hasta el ultimo valor"
 
 print(nombre_curso[:])
--- Tipos/04-metodosstring.py
+++ Tipos/04-metodosstring.py
@@ -1,48 +1,47 @@
 animal = "chAnchito Feliz"
 animal2 =" chanchito triste "
 
-"Convertir todo a mayuscula"
+#Convertir todo a mayuscula
 
 print(animal.upper())
 
-"Convertir todo a minuscula"
+#Convertir todo a minuscula
 
 print(animal.lower())
 
-"Primer elemento mayuscula el resto minuscula"
+#Primer elemento mayuscula el resto minuscula
 
 print(animal.capitalize())
 
-"Las primeras letras de las palabras separadas tiene mayuscula"
+#Las primeras letras de las palabras separadas tiene mayuscula
 
 print(animal2.title())
 
-"Borrar espacio de los extremos"
+#Borrar espacio de los extremos
 
 print(animal2.strip())
 
-"Se pueden usar dos funciones al mismo tiempo"
+#Se pueden usar dos funciones al mismo tiempo
 
 print(animal2.strip().capitalize())
 
-
-"Borrar espacio de la derecha"
+#Borrar espacio de la derecha
 
 print(animal2.rstrip())
 
-"Borrar espacio de la izquierda"
+#Borrar espacio de la izquierda
 
 print(animal2.lstrip())
 
-"Buscar cadena de caracteres / si es -1 es que no existe"
+#Buscar cadena de caracteres / si es -1 es que no existe
 
 print(animal.find("it"))
 
-"Reemplazar un valor"
+#Reemplazar un valor
 
 print(animal.replace("ch","p"))
 
-"Devolver un boleano"
+#Devolver un boleano
 
 print("ch" in animal)
 
